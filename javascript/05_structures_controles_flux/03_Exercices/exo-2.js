67428d721340f1fc4df4c3e596490b8b8dde0c69
MrScrupulus <mr.scrupulus@gmail.com>
2024-08-02 16:35:14 +0200

    exos_js

--- javascript/05_structures_controles_flux/03_Exercices/exo-2.js
+++ javascript/05_structures_controles_flux/03_Exercices/exo-2.js
@@ -1,9 +1,12 @@
 // Exercice 2 :
-let couleur = "rouge";
+let couleur = "orange";
 //Untilisez une instruction "switch" pour vérifier la valeur d'une variable "couleur".
 switch (couleur) {
   case "rouge":
     console.log("rouge signifie danger");
+    break;
+  case "orange":
+    console.log("orange signifie risque");
     break;
   case "vert":
     console.log("vert signifie sécurité");
@@ -11,3 +14,41 @@ switch (couleur) {
 }
 
 //affichez la significaation correspondante (par exemple "rouge" signifie "danger", vert signifie "sécurité", etc)
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const recursiveAsyncReadLine = function () {
+  rl.question(
+    "Veuillez saisir une couleur ou quitter ['Au revoir'] : ",
+    function (couleur) {
+      if (couleur === "Au revoir") {
+        console.log("Bisous");
+        return rl.close(); //closing RL and returning from function.
+      }
+      //we need some base case, for recursion
+
+      switch (couleur) {
+        case "vert":
+          console.log("sécurisé");
+          break;
+        case "jaune":
+          console.log("attention");
+          break;
+        case "rouge":
+          console.log("dangereux");
+          break;
+        default:
+          console.log("couleur non reconnue! ['rouge', 'vert', 'jaune']");
+          break;
+      }
+      recursiveAsyncReadLine(); //Calling this function again to ask new question
+    }
+  );
+};
+
+recursiveAsyncReadLine(); //we have to actually start our recursion somehow
