66f1a88047894e18d9f1088ab697acd514d1e946
MrScrupulus <mr.scrupulus@gmail.com>
2024-08-02 16:05:48 +0200

    exos js

---
+++ javascript/05_structures_controles_flux/03_Exercices/exo-2.js
@@ -0,0 +1,13 @@
+// Exercice 2 :
+let couleur = "rouge";
+//Untilisez une instruction "switch" pour vérifier la valeur d'une variable "couleur".
+switch (couleur) {
+  case "rouge":
+    console.log("rouge signifie danger");
+    break;
+  case "vert":
+    console.log("vert signifie sécurité");
+    break;
+}
+
+//affichez la significaation correspondante (par exemple "rouge" signifie "danger", vert signifie "sécurité", etc)
