873783b8d8394f3c976d6eeb845b8175cb49adcf
Emile D <mr.scrupulus@gmail.com>
2024-08-05 20:09:05 +0200

    assurance

---
+++ exo_assurance/index.js
@@ -0,0 +1,38 @@
+let
+
+
+
+
+let age = 25;
+if (age < 25) {
+  console.log("+1");
+} else if (age > 25) {
+  console.log("-1");
+}
+
+let permis = 2;
+if (permis < 2) {
+  console.log("+1");
+} else if (permis > 2) {
+  console.log("-1");
+}
+
+let accident = 0;
+if (accident < 0) {
+  console.log("-1");
+} else if (accident > 0) {
+  console.log("+1");
+} else if (accident == +3) {
+  console.log("+3");
+}
+
+
+let a = 
+function addition(a, b) {
+  let result = a + b;
+  return result;
+
+console.log(addition(1, 4));
+console.log(addition(1, 9));
+console.log(addition(1, 15))
+}
